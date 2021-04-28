78cc90b688d6c55d9d26c87391be85b882d55679
fedkendi <fedked@outlook.com>
2021-04-28 14:00:05 -0300

    adicionado projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'my-app';
+
+export class AppComponent implements OnInit {
+
+  title = 'angular-dark-theme-toggle';
+  storedTheme = localStorage.getItem('theme-color');
+
+  constructor() {}
+  ngOnInit() {}
+
+  setTheme() {
+    if (this.storedTheme === 'theme-dark') {
+      // toggle and update Local storage
+      localStorage.setItem('theme-color', 'theme-light');
+      this.storedTheme = localStorage.getItem('theme-color');
+    } else {
+      // toggle and update local storage
+      localStorage.setItem('theme-color', 'theme-dark');
+      this.storedTheme = localStorage.getItem('theme-color');
+    }
+  }
 }
